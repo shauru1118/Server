0e71f8006f593b41077e8a304a93cf8d82e6b4bf
Albom3 <sargsyan_ae09@mail.ru>
2025-05-01 19:12:45 +0300

    port

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def serve_page(filename):
 
 if __name__ == "__main__":
     print(VIEWS_DIR)
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)

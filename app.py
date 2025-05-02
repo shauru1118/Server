d13cf765fe42cab1e9c7db4df0ad584552edce4d
Albom3 <sargsyan_ae09@mail.ru>
2025-05-02 23:57:58 +0300

    new venv

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
-from flask import Flask, send_from_directory, abort
+from flask import Flask, send_from_directory, abort, render_template, url_for
 import os
+from time import sleep
 
 App = Flask(__name__)
 
-VIEWS_DIR = os.path.join(os.path.dirname(__file__), "views")
+VIEWS_DIR = os.path.join(os.path.dirname(__file__), "templates")
 
 
-@App.route("/app")
-def app():
-    return "App success."
 
 @App.route("/")
 @App.route("/home")
-def home():
-    return send_from_directory(VIEWS_DIR, "index.html")
+def index():
+    return render_template("index.html")
+
+@App.route("/app")
+def app():
+    return render_template("app.html")
+
+@App.route("/not_found  ")
+def not_found():
+    return render_template("not_found.html"), 404
+
+@App.route("/user/<username>/<id>")
+def user(username, id):
+    return f"<h1>Hello |{username}| with id |{id}| !</h1>"
 
 @App.route("/<path:filename>")
 def serve_page(filename):
     filepath = os.path.join(VIEWS_DIR, filename)
     if os.path.exists(filepath):
-        return send_from_directory(VIEWS_DIR, filename)
+        return render_template(filename)
     else:
-        return send_from_directory(VIEWS_DIR, "404.html"), 404
+        return render_template("not_found.html"), 404
 
 if __name__ == "__main__":
     print(VIEWS_DIR)
     port = int(os.environ.get("PORT", 8080))
-    App.run(host="0.0.0.0", port=port)
+    
+    while True:
+        App.run(host="0.0.0.0", port=port, debug=True)
